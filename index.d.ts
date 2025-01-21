39eeaf9dff6951ba9f7f34421263fc712cca3eec
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2025-01-21 15:32:40 +0100

    Fix wrong function name declared in `index.d.ts`
    :facepalm:

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@ type Defined<T> = {
     [K in keyof T as T[K] extends null | undefined ? never : K]: T[K];
 }
 
-export function defined<T extends object>(value: T): Defined<T>;
+export function omitUndefined<T extends object>(value: T): Defined<T>;
